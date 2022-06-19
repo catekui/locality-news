d41ff78c87b9a13b35b0a364a608407b92e6b69c
catekui <catherine.wanjiku@student.moringaschool.com>
2022-06-19 21:04:11 +0300

    created class Neighbourhood

--- Neiba/models.py
+++ Neiba/models.py
@@ -16,4 +16,42 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to = 'media/', null = True, blank = True)
+    about = models.TextField(max_length=200, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cop_dail = models.IntegerField(null=True, blank=True)
+    health_dail = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def create_hood(self):
+        self.save()
+
+    @classmethod
+    def delete_hood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    @classmethod
+    def find_hood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    @classmethod
+    def update_hood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def update_occupants(cls, occupants_count):
+        cls.objects.filter(occupants_count=occupants_count).update()
     
